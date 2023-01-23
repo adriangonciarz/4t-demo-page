dd8108b835412acbd64f53c4d5541b5164a9d04d
Adrian Gonciarz <adrian.gonciarz@gmail.com>
2023-01-23 17:39:25 +0100

    feat: use Postgres DB

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -79,13 +79,17 @@ WSGI_APPLICATION = 'eCommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '4tdemostore',
+        'USER': '4tdemostore',
+        'PASSWORD': 'complexpassword123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
